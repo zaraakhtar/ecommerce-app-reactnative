78453db109fa9e89389c781838ce505fa38cf6cd
zaraakhtar <zaramalikawan9@gmail.com>
2025-07-18 21:38:13 +0500

    changed the home page

--- App.tsx
+++ App.tsx
@@ -7,14 +7,16 @@
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Text } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text>
+        this is my ecommerce app
+      </Text>
     </View>
   );
 }
