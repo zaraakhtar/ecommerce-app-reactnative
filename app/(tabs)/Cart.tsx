038ce70770f1578283ad294aa17933559e8a142a
zaraakhtar <zaramalikawan9@gmail.com>
2025-07-27 23:03:28 +0500

    new files tabs created

---
+++ app/(tabs)/Cart.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const cartScreen = () => {
+  return (
+    <View>
+      <Text>cartScreen</Text>
+    </View>
+  )
+}
+
+export default cartScreen
+
+const styles = StyleSheet.create({})
--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -4,14 +4,19 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Logo from "./logo";
 import AppColors from "@/constants/Colors";
 import { AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const HomeHeader = () => {
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Logo />
         <View style={styles.iconContainer}>
-          <TouchableOpacity>
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => router.push("/")}
+          >
             <AntDesign
               name="search1"
               size={20}
@@ -41,8 +46,20 @@ const styles = StyleSheet.create({
   },
   iconContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-end'
-  }
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  searchButton: {
+    backgroundColor: AppColors.primary[50],
+    borderRadius: 5,
+    height: 35,
+    width: 35,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 10,
+    borderWidth: 1,
+    borderColor: AppColors.primary[500],
+    position: "relative",
+  },
 });
